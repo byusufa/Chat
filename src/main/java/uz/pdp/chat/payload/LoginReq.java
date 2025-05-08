afa12b091acca685954a22d8179c2452508ac444
byusufa <jozef12079104293@gmail.com>
2025-05-08 18:12:44 +0500

    LoginRequest commit

---
+++ src/main/java/uz/pdp/chat/payload/LoginReq.java
@@ -0,0 +1,15 @@
+package uz.pdp.chat.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginReq {
+
+    private String phone;
+    private String password;
+
+}
